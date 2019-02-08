090584b0923b9984725d6588419d000b652835ab
Ramyamanob <47448974+Ramyamanob@users.noreply.github.com>
2019-02-08 19:10:56 +0530

    Update and rename README.md to pr.c

---
+++ pr.c
@@ -0,0 +1,16 @@
+# c.pr
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%4==0)
+    {
+        printf("is leap year");
+    }
+    else
+    {
+        printf("is not a leap year");
+    }
+    return 0;
+}
